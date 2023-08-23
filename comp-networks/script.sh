8688ffc4eb5b0d1e2acc727f2862cb2d946f31b8
HariVamshi6000 <ai21btech11014@iith.ac.in>
2023-08-23 11:23:43 +0530

    added new servers - donkey's PC

--- comp-networks/script.sh
+++ comp-networks/script.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+hostname="`hostnamectl| grep -i hostname | grep -i static`"
+if [ -d "./traceroutes/${hostname:20}" ]; then
+   echo "lmao" > /dev/null 
+else
+    mkdir ./traceroutes/"${hostname:20}"
+fi
+
 personal_ip="random ass text (`curl ifconfig.me`)"
-hostname="`hostnamectl| grep hostname`"
 for line in `strings ./lines.txt`
 do 	
-	echo $personal_ip > ./traceroutes/"${hostname:18}""${line:4 : -4}".txt
-	traceroute -I $line >> ./traceroutes/"${hostname:18}""${line:4 : -4}".txt
+	echo $personal_ip > ./traceroutes/"${hostname:20}"/"${line:4 : -4}".txt
+	traceroute -I $line >> ./traceroutes/"${hostname:20}"/"${line:4 : -4}".txt
 done
 
 exit
