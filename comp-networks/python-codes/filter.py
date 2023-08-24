5595c74bc5ee1e49f1f18001a5bd93b085336d16
AP-51 <ai21btech11002@iith.ac.in>
2023-08-25 02:19:01 +0530

    updated filter.py

--- comp-networks/python-codes/filter.py
+++ comp-networks/python-codes/filter.py
@@ -2,21 +2,33 @@ import os
 import json 
 
 p=open('private.py','w')
-file=open('output.json')
 
+dir_path="./"
+directory=os.fsencode(dir_path)
 array=[]
-data=json.load(file)
-for ip in data:
-    if(ip["status"]=="fail"):
-        array.append(ip["query"])
-#print(array)
+pretty_ip=[]
 
+for subdir,dirs,files in os.walk(directory):
+    for file in files:
+        filename = os.fsdecode(file)
+        if "output-" in filename:
+            file=open(filename)
+            data=json.load(file)
+            for ip in data:
+                if(ip["status"]=="fail"):
+                    array.append(ip["query"])
+            if len(pretty_ip)<1:
+                pretty_ip=data
+            else:
+                pretty_ip.append(data)
+            file.close()
+#print(array)
+print(pretty_ip)
 p.writelines(f"arr={array}\n")
-pretty_data=json.dumps(data,indent=4)
+pretty_data=json.dumps(pretty_ip,indent=4)
 
 with open("../../pretty_ip.json","w") as outfile:
     outfile.write(pretty_data)
 
 
 p.close()
-file.close()
